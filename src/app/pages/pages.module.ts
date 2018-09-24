d3a57a603d898e45010c9fb7c4973f261257065e
jarriagada <jarriagada@bancointernacional.cl>
2018-09-24 15:09:27 -0300

    Agregamos y modificaciones modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+
+//Component
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+//Module
+import { SharedModule } from '../shared/shared.module';
+
+//Rutas
+import { PAGES_ROUTES } from './pages.routes';
+
+
+
+@NgModule({
+    declarations:[
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports : [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule { }
